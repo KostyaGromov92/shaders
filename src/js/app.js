142ab5977d62c41d6b18e897c64a2f9edf2db3e0
Kostya Gromov <gromovkostya92@gmail.com>
2018-02-12 16:15:19 +0200

    Update project

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,52 @@
-import sayHello from './lib/sayHello.js';
+import * as THREE from 'three';
+var OrbitControls = require('three-orbit-controls')(THREE);
 
-sayHello();
+
+
+
+
+
+var camera, controls, scene, renderer, mesh;
+
+function init() {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x000000);
+  // scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+
+  renderer = new THREE.WebGLRenderer();
+
+
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerWidth);
+
+  var container = document.getElementById('container');
+  container.appendChild(renderer.domElement);
+
+  camera = new THREE.PerspectiveCamera(
+    90,
+    window.innerWidth / window.innerHeight,
+    1,
+    3000
+  );
+  camera.position.z = 200;
+
+  controls = new OrbitControls(camera, renderer.domElement);
+
+
+  // do something
+
+  animate();
+}
+
+
+
+let time = 0;
+function animate() {
+  time++;
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+
+init();
